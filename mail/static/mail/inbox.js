b1206b69080e9821365ed860d4b475a4792b6b6b
mdhcodes <mdhcodes@gmail.com>
2024-04-23 11:28:50 -0400

    Complete the compose_email js function to send an email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,6 +20,44 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  // Send email
+  // When the 'SubmitQuery' Button is clicked...
+  // const submit_btn = document.addEventListener('input', () => {}) // Did not test the 'input' event listener.
+  // https://stackoverflow.com/questions/20840128/how-can-i-select-a-button-with-a-type-of-submit-using-javascript
+  const submit_btn = document.querySelector('input[type="submit"]');
+
+  // Getting the following error in Firefox: TypeError: NetworkError when attempting to fetch resource.
+  // With Chrome, I received the above error but the data did post to the database.
+  // To correct the error in Firefox and Chrome, I had to prevent the default form submission.
+  // https://stackoverflow.com/questions/59176488/networkerror-when-attempting-to-fetch-resource-only-on-firefox
+  // https://stackoverflow.com/questions/30473581/when-to-use-preventdefault-vs-return-false 
+  submit_btn.addEventListener('click', (e) => {
+    e.preventDefault(); // Prevent form submission
+
+    email_recipients = document.querySelector('#compose-recipients').value;
+    email_subject = document.querySelector('#compose-subject').value;
+    email_body = document.querySelector('#compose-body').value;
+
+    // Send a POST request to the /emails route with the values captured above. 
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: email_recipients,
+        subject: email_subject,
+        body: email_body
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log('Result:', result);
+      // Once the email has been sent, load the user’s sent mailbox.
+      load_mailbox('sent')
+    })
+    .catch((error) => {
+      console.log("Error:", error)
+    });
+  })  
 }
 
 function load_mailbox(mailbox) {
--- mail/urls.py
+++ mail/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path("register", views.register, name="register"),
 
     # API Routes
+    # https://docs.djangoproject.com/en/5.0/topics/http/urls/#naming-url-patterns
+    # https://stackoverflow.com/questions/51922641/what-is-the-function-of-the-name-parameter-in-django-urls-path
+    # Send email requires a POST request to emails. The form action url in inbox.html is the path name 'compose'. 
     path("emails", views.compose, name="compose"),
     path("emails/<int:email_id>", views.email, name="email"),
     path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
