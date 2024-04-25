c943d9b0e9693d3c15855589014957610c4367c3
mdhcodes <mdhcodes@gmail.com>
2024-04-25 08:27:27 -0400

    Add functionality to archive emails

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -78,21 +78,33 @@ function view_email(email) {
       const h2 = document.createElement('h2');
       h2.innerHTML = `${email.subject}`;
       const p1 = document.createElement('p');
-      p1.innerHTML = `From: ${email.sender}`;                 
+      p1.innerHTML = `From: ${email.sender}`;           
       const p2 = document.createElement('p');
       p2.innerHTML = `To: ${email.recipients}`;  
       const p3 = document.createElement('p');
-      p3.innerHTML = `Date: ${email.timestamp}`;                 
+      p3.innerHTML = `Subject: ${email.subject}`;                       
       const p4 = document.createElement('p');
-      p4.innerHTML = `${email.body}`;       
+      p4.innerHTML = `Date: ${email.timestamp}`;
+      const hr = document.createElement('hr');                      
+      const p5 = document.createElement('p');
+      p5.innerHTML = `${email.body}`;             
 
       email_view_div.append(h2);
       email_view_div.append(p1);
       email_view_div.append(p2);
       email_view_div.append(p3);
       email_view_div.append(p4);
+      email_view_div.append(hr);
+      email_view_div.append(p5);
 
   }); 
+}
+
+function archive(emails) {
+  // console.log('Archive function:', emails);
+
+  
+
 }
 
 function load_mailbox(mailbox) {
@@ -112,7 +124,12 @@ function load_mailbox(mailbox) {
       // https://cs50.harvard.edu/web/2020/notes/5/#arrow-functions - this keyword
       // console.log('This value:', this.innerHTML);
       mailbox = (this.innerHTML).toLowerCase();
-      console.log('Mailbox:', mailbox)
+      // console.log('Mailbox:', mailbox)
+
+      // Match string in mailbox() function in views.py.
+      if (mailbox === 'archived') {
+        mailbox = 'archive';
+      } 
 
       // When a mailbox is visited, the application should first query the API for the latest emails in that mailbox.  
       // Send a GET request to /emails/<mailbox> to request the emails for a particular mailbox.
@@ -121,7 +138,7 @@ function load_mailbox(mailbox) {
       .then(response => response.json())
       .then(emails => {
           // Print emails
-          console.log(emails);
+          // console.log(emails);
       
           // ... do something else with emails ...
           // Each email is rendered in its own box (e.g. as a <div> with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
@@ -153,16 +170,29 @@ function load_mailbox(mailbox) {
           for (const email of emails) {
             // console.log('Email:', email);            
             
-            const table_row = document.createElement('tr');
+            const table_row = document.createElement('tr'); // Don't need to add a link since the click on the row triggers the event.
             // When a user clicks on an email, the user is taken to a view where they see the content of that email. Add links to each email.
-            const email_link = document.createElement('a');
+            // const email_link = document.createElement('a');
+
             // email link needs a url (email) and an argument (id) - https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
             // email_link.setAttribute('href', 'email' + email.id);
             // BOTH email_link.setAttribute() AND email_link.href WORK.
             // https://stackoverflow.com/questions/4689344/how-can-i-add-href-attribute-to-a-link-dynamically-using-javascript
-            email_link.href = 'email' + email.id;
-            // Add event listener (click) to the table row
-            table_row.addEventListener('click', function() {
+            // email_link.href = 'email' + email.id;
+            // Add event listener (click) to the table row - CONFLICTS WITH ARCHIVE BUTTON.
+            
+            table_row.addEventListener('click', function(event) {
+
+              // https://stackoverflow.com/questions/43946330/overlapped-elements-with-onclick-event
+              // https://www.w3schools.com/jsref/event_stoppropagation.asp
+              // Removes the conflict and stops the table_row event from being triggered.
+              event.stopPropagation();
+            
+            // Add click event to table data to fix conflict
+
+              
+              // const show_email = document.querySelector('.show-email');
+              // show_email.addEventListener('click', function() {
               // console.log('This email was clicked!');
 
               // Clear contents of the email-view div. 
@@ -187,12 +217,19 @@ function load_mailbox(mailbox) {
             } else {
               // If the email has been read, it should appear with a gray background.
               table_row.classList.add('table-secondary');
-            }    
+            } 
+               
 
             const table_data_1 = document.createElement('td');
             const table_data_2 = document.createElement('td');
             const table_data_3 = document.createElement('td');
             const table_data_4 = document.createElement('td');
+            // Add class to trigger show email without conflicting with the archive button.
+            table_data_1.classList.add('show-email');
+            table_data_2.classList.add('show-email');
+            table_data_3.classList.add('show-email');
+            table_data_4.classList.add('show-email');
+
             table_data_1.innerHTML = `${email.sender}`;
             table_data_2.innerHTML = `${email.recipients}`;
             table_data_3.innerHTML = `${email.subject}`;
@@ -203,7 +240,62 @@ function load_mailbox(mailbox) {
             table_row.append(table_data_1);
             table_row.append(table_data_2);
             table_row.append(table_data_3);
-            table_row.append(table_data_4);            
+            table_row.append(table_data_4);  
+            
+            // When viewing an Inbox email, the user should be presented with a button that lets them archive the email. 
+            // When viewing an Archive email, the user should be presented with a button that lets them unarchive the email. 
+            if (mailbox === 'inbox' || mailbox === 'archive') {  
+              // Create a button to archive email(s)
+              const archive_button = document.createElement('button');
+
+              if (email.archived === false) {
+                archive_button.innerHTML = 'Archive';  
+              } else {
+                archive_button.innerHTML = 'Unarchive';
+              }
+
+              // Adding the button to the row causes the click event to trigger the email to display not the inbox.
+              table_row.append(archive_button);
+          
+              archive_button.addEventListener('click', (event) => {
+                
+                // https://stackoverflow.com/questions/43946330/overlapped-elements-with-onclick-event
+                // https://www.w3schools.com/jsref/event_stoppropagation.asp
+                // Removes the conflict and stops the table_row event from being triggered.
+                event.stopPropagation();              
+                                
+                if (email.archived === false) {
+                  // Send a PUT request to /emails/<email_id> to mark an email as archived or unarchived.
+                  fetch(`/emails/${email.id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        archived: true
+                    })
+                  }).then(
+                    // Once an email has been archived, load the user’s inbox. 
+                    // !!!!!! The inbox loads but no emails are populated. !!!!!!
+                    () => {
+                      load_mailbox('inbox');
+                    }
+                  )
+                } else {
+                  fetch(`/emails/${email.id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        archived: false
+                    })
+                  })
+                  .then(
+                    // Once an email has been unarchived, load the user’s inbox. 
+                    // !!!!!! The inbox loads but no emails are populated. !!!!!!
+                    () => {
+                      load_mailbox('inbox');
+                    }
+                  )
+
+                }                  
+              });
+            }
           }
       })
       .catch((error) => {
@@ -211,4 +303,4 @@ function load_mailbox(mailbox) {
       });
     }
   })
-}
+}                            
